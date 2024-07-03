5ca42f07272a522f08d30a395950d62482acb374
eymatsuda2016 <eymatsuda2016@outlook.com>
2024-07-03 20:27:39 -0300

    Projeto Inicial

---
+++ src/main/java/com/eduardomatsuda/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,27 @@
+package com.eduardomatsuda.cursomc.domain;
+
+import com.eduardomatsuda.cursomc.domain.enums.EstadoPagamento;
+
+public class PagamentoComCartao extends Pagamento {
+	private static final long serialVersionUID = 1L;
+
+	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		this.setNumeroDeParcelas(numeroDeParcelas);
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+	
+	
+}
