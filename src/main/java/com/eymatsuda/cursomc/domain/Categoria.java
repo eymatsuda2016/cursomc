52c713151392b2a082b831559dc80eb50913fa3c
eymatsuda2016 <eymatsuda2016@outlook.com>
2025-05-05 16:34:22 -0300

    Banco de Dados H2 e Criação Automática da Base de Dados

--- src/main/java/com/eymatsuda/cursomc/domain/Categoria.java
+++ src/main/java/com/eymatsuda/cursomc/domain/Categoria.java
@@ -3,9 +3,17 @@ package com.eymatsuda.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	private String nome;
 	
